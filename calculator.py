4445bb33406a219236b52f76efbfc4846199659b
A Valembrun <bruntime@gmail.com>
2015-01-13 00:21:56 -0500

    Water Prepping Calculator
    dynamic survival calculator for any number of people and any number of
    months

--- calculator.py
+++ calculator.py
@@ -1,14 +1,21 @@
-#Water Prepping Calculator for 1 Month
+#Water Prepping Calculator
 
 water_gallon = 30
-months = 1
 
 print "1 Person needs", water_gallon, " gallons of water to survive for 1 month"
 
-water_amt = int(raw_input("How much water do you have? "))
-water_total = (months * water_gallon) - water_amt
+mths_of_survival = int(raw_input("How many months will you need to survive? "))
 
-if water_total > 0:
-	print "You do not have enough water. You need %d more gallons of water" % (water_total)
+person = int(raw_input("How many people will need water? "))
+
+total_water_need = person * (mths_of_survival * water_gallon)
+
+current_water_total = int(raw_input("How much water do you have? "))
+water_missing = total_water_need - current_water_total
+
+if water_missing < 0:
+	print "You have MORE than enough water to survive %d months of a disaster" % (mths_of_survival)
+elif water_missing == 0:
+	print "You have just enough water to last you %d months during the apocalypse" %(mths_of_survival)
 else:
-	print "You have more than enough water"
+	print "I'm not sure you'll make it. You need %d more gallons of water." %(water_missing)
