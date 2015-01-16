219f3cb7d65f94e136c15e0ed784130b1958bc12
A Valembrun <bruntime@gmail.com>
2015-01-15 23:45:39 -0500

    Bug Fixes
    fixed few issues from previous version.
    
    Issues in previous version:
    
    - user allowed 9 guesses instead of 8
    - if user got answer right on the last guess, they received an error
    message
    - user could input numbers outside of guessing range

--- Guess_v2.py
+++ Guess_v2.py
@@ -4,22 +4,25 @@ import random
 
 number = random.randint(0, 75)
 num_guess = 8
+guess_total = 2 #initializes at 2 because technically first guess is not within loop
 
 print ("I'm thinking of a number between 0 and 75. You have %d guesses.") %(num_guess)
 
 user_guess = int(raw_input("What's your guess? "))
 
-guess_total = 0
-
 while guess_total < num_guess:
 	guess_total += 1
+	if user_guess < 0 or user_guess > 75:
+		print ("That number is not within range.")
 	if user_guess < number:
 		print ("That's incorrect. Go higher.")
 		user_guess = int(raw_input("Guess again. "))
-	elif user_guess > number:
+	if user_guess > number:
 		print ("That's incorrect. Lower.")
 		user_guess = int(raw_input("Guess again. "))
-	elif user_guess == number:
+	if guess_total == 8:
+		user_guess = int(raw_input("This is your last guess. "))
+	if user_guess == number:
 		print ("You're correct!!!!")
 		break
 else:
