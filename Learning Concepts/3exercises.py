ae32c87445f5ecb733dc889c9cc9f17445493881
A Valembrun <bruntime@gmail.com>
2015-01-26 22:44:35 -0500

    Continue Exercises - Learning Concepts (Python)
    - Fixed issue that was causing error message (created function without
    using built in len() function)
    - Exercise of returning largest number (without max() function), used
    list, a little verbose
    - noticed issue with max() function, not sure why - included image,
    going to find out why that happens (between both versions, list is most
    accurate)

--- Learning Concepts/3exercises.py
+++ Learning Concepts/3exercises.py
@@ -1,10 +1,29 @@
 #Define a function max_of_three() that takes three numbers and returns the largest of them.
 
 def max_of_three():
+
+#with max() function
+
 	num1 = raw_input("First number: ")
 	num2 = raw_input("Second number: ")
 	num3 = raw_input("Third number: ")
 	print "Highest number is", max (num1, num2, num3)
+
+#without max() function	
+
+	numbers = []
+	
+	new_num1 = raw_input("First number: ")
+	numbers.append(new_num1)
+	new_num2 = raw_input("Second number: ")
+	numbers.append(new_num2)
+	new_num3 = raw_input("Third number: ")
+	numbers.append(new_num3)
+
+	numbers = [int(x) for x in numbers]
+	numbers.sort()
+	
+	print numbers[-1]
 
 #Define a function that computes the length of a given string.
 
@@ -15,21 +34,20 @@ def count_string():
 	print "Write something here"
 	string1 = raw_input("")
 
-	print "Number of characters in this sentence", len(string1)
+	print "Number of characters in this 1st sentence", len(string1)
 	
 #without using the built in len() function
-
-#first attempt: TypeError: cannot concatenate 'str' and 'int' objects
 	
 	print "Write more here"
-	
-	x = 0
-	
+		
 	string2 = raw_input("")
 	
+	str_length = 0
+	
 	for x in string2:
-		x += 1
-		print "Number of characters in this sentence", x
+		str_length += 1
+
+	print "Number of characters in this 2nd sentence", str_length
 		
 #Write a function that takes a character (i.e. a string of length 1) and returns True if it is a vowel, False otherwise.
 
