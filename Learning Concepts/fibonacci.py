dc58d9676bed047ac9653e6eac4ebe9c76200dc6
A Valembrun <bruntime@gmail.com>
2015-01-29 10:48:38 -0500

    Fibonacci Continued
    Even numbers extracted

--- Learning Concepts/fibonacci.py
+++ Learning Concepts/fibonacci.py
@@ -5,12 +5,20 @@ list_of_nums = [0, 1]
 a = -1
 b = 0
 
-while b < 10:
+while list_of_nums[-1] <4000000:
 
 	a += 1
 	b += 1
 	
 	sum = list_of_nums[a] + list_of_nums[b]
-	
 	list_of_nums.append(sum)
+	
 print list_of_nums
+
+for x in list_of_nums:
+	if x % 2 == 0:
+		print x
+
+
+
+
