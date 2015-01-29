9c41b345ee8bfa76bd41de9fbc0b86bd2bbba766
A Valembrun <bruntime@gmail.com>
2015-01-29 00:42:55 -0500

    Fibonacci Exercise (Python) v2
    - fixed error in exercise upon figuring out how to access list elements
    - also noticed using wrong symbols, needed to use brackets not
    parantheses - [a] [b] instead of (a) (b)
    - next part: even Fibonacci numbers - PE exercise 2

--- Learning Concepts/fibonacci.py
+++ Learning Concepts/fibonacci.py
@@ -2,18 +2,15 @@
 
 list_of_nums = [1, 2]
 
-a = 0
-b = 1
+a = -1
+b = 0
 
 while b < 10:
 
 	a += 1
 	b += 1
 	
-#Expected Result: Sum = number in index a + number in index b
-#Actual Result: Sum = list index a + list index b, Error
-
-	sum = list_of_nums.index(a) + list_of_nums.index(b)
+	sum = list_of_nums[a] + list_of_nums[b]
 	
 	list_of_nums.append(sum)
 	print list_of_nums
