fd9a135665da06b9a8f0cd6ad1e67261724643d1
A Valembrun <bruntime@gmail.com>
2015-02-01 22:43:28 -0500

    Exercise 1
    accept user input

---
+++ 2monkeys.py
@@ -0,0 +1,21 @@
+#Exercise: 2 Monkeys
+
+# We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble. 
+
+
+def monkey_trouble(a_smile, b_smile):
+
+	if a_smile == "Yes" and b_smile == "Yes":
+		print "Both Smiling"
+	    # return True
+		if not a_smile == "No" and not b_smile == "No":
+			print "Neither smiling"
+	    # return True
+	else:
+		print "One is smiling"
+	    # return False
+	
+answer1 = raw_input("Is Monkey 1 smiling? ")
+answer2 = raw_input("Is Monkey 2 smiling? ")
+
+monkey_trouble(answer1, answer2)
