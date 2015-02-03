2ec9dc2e8d2e6e0b58da250f9434fdb2f41f6b7c
A Valembrun <bruntime@gmail.com>
2015-02-02 21:42:19 -0500

    PE Problem 3 (Python)
    - Finally!
    - Works but uses a lot of memory
    - Revision necessary, update in future

--- PE/3. primefactor.py
+++ PE/3. primefactor.py
@@ -5,23 +5,26 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143?
 
-a = 1
-check_num = 2000
 factors = []
-prime_num = []
+
+def is_prime(n):
+	
+	a = 0
+	while a <= n:
+		a += 1
+		if n % a == 0:
+		 factors.append(a)
+	if len(factors) > 2:
+		return False
+	else:
+		return True
+
+a = 1
+check_num = 600851475143
 
 while a <= check_num:
 	a +=1
 	if check_num % a == 0:
-		factors.append(a)
-print factors
-
-i = 0
-
-x for x in factors:
-	while i != factors[-1]:
-		i += 1
-		if (x / i) == 1:
-			prime_num.append(x)		
-print prime_num
-
+		if is_prime(a):
+			factors.append(a)
+print factors[-1]
