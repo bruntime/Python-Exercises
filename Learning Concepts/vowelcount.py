d2bc9765fcb2b640dd2719652bdf6b6f55e103c9
A Valembrun <bruntime@gmail.com>
2015-02-05 01:00:24 -0500

    Exercise - Vowel Count (Python)
    - changes, not sure why it is not working .... what am I missing?

--- Learning Concepts/vowelcount.py
+++ Learning Concepts/vowelcount.py
@@ -1,15 +1,13 @@
 def VowelCount(str): 
-
   
 	vowels = ['a', 'e', 'i', 'o', 'u']
   
 	count = 0
-  
-	# for i in vowels:
-	if i in vowels:
-		for i in str:
+
+	for vowels in vowels:
+		if vowels in str:
 			count += 1
-    
+	
 usertxt = raw_input("Some random text: ")
 	
 print VowelCount(usertxt)  
