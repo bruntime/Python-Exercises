157fa8346c03f10422a2eb44eb534bc6e2483e47
A Valembrun <bruntime@gmail.com>
2015-02-04 23:59:28 -0500

    PE Problem 10 (Python)
    - divided list of prime numbers to sum up later (still not a good
    solution - just temporary)

--- PE/10. sum of primes.py
+++ PE/10. sum of primes.py
@@ -23,21 +23,56 @@ list_num1 = []
 list_num2 = []
 list_num3 = []
 list_num4 = []
+list_num5 = []
+list_num6 = []
+list_num7 = []
+list_num8 = []
+list_num9 = []
+list_num10 = []
+list_num11 = []
 
-for num in xrange (1, 500000):
+for num in xrange (1, 200000):
 	if is_prime(num):
 		list_num1 = list_num1 + [num]
-		
-for num in xrange (500000, 1000000):
+
+for num in xrange (200000, 400000):
 	if is_prime(num):
 		list_num2 = list_num2 + [num]
-
-for num in xrange (1000000, 1500000):
+		
+for num in xrange (400000, 600000):
 	if is_prime(num):
 		list_num3 = list_num3 + [num]
 
-for num in xrange (1500000, 2000000):
+for num in xrange (600000, 800000):
 	if is_prime(num):
-		list_num4 = list_num4 + [num]	
-		
-print sum(list_num1 + list_num2 + list_num3 + list_num4)
+		list_num4 = list_num4 + [num]
+
+for num in xrange (800000, 1000000):
+	if is_prime(num):
+		list_num5 = list_num5 + [num]
+
+for num in xrange (1000000, 1200000):
+	if is_prime(num):
+		list_num6 = list_num6 + [num]
+
+for num in xrange (1200000, 1400000):
+	if is_prime(num):
+		list_num7 = list_num7 + [num]
+
+for num in xrange (1200000, 1400000):
+	if is_prime(num):
+		list_num8 = list_num8 + [num]
+
+for num in xrange (1400000, 1600000):
+	if is_prime(num):
+		list_num9 = list_num9 + [num]
+
+for num in xrange (1600000, 1800000):
+	if is_prime(num):
+		list_num10 = list_num10 + [num]
+
+for num in xrange (1800000, 2000000):
+	if is_prime(num):
+		list_num11 = list_num11 + [num]
+
+print sum(list_num1 + list_num2 + list_num3 + list_num4 + list_num5 + list_num6 + list_num7 + list_num8 + list_num9 + list_num10 + list_num11)
