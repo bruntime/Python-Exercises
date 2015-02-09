02878ae1a9c69b0868084a13a9b5d75ba340d13c
A Valembrun <bruntime@gmail.com>
2015-02-08 23:12:07 -0500

    Exercise - Palindrome (in progress)
    - Trying to figure out why program isn't working as expected
    - Just learned that there's no reverse built-in function for strings in
    Python, got to figure out a workaround

--- Learning Concepts/palindrome.py
+++ Learning Concepts/palindrome.py
@@ -1,15 +1,27 @@
 # Have the function Palindrome(str) take the str parameter being passed and return the string true if the parameter is a palindrome, (the string is the same forward as it is backward) otherwise return the string false. For example: "racecar" is also "racecar" backwards. Punctuation and numbers will not be part of the string. 
 
+import sys
+
 def Palindrome(str):
 
-	if str == str.reverse():
+	str = None
+
+	#if str == str.reverse(): #this does not work because Python does not have a built-in reverse function for Strings
+	# new_str = str.split( )
+	# new_str = (reverse(str))
+	# new_str = join(reverse(str))
+	if str == new_str:
 		return "true"
 	else:
 		return "false"
 
 txt = raw_input("Test if palindrome: ")		
 
-Palindrome(txt)
+new_txt = txt.split()
+txt_reverse = new_txt.reverse()
+print txt_reverse #message: None - not expected outcome
+
+# Palindrome(txt)
 
 # http://www.coderbyte.com/CodingArea/Editor.php?ct=Palindrome&lan=Python
 
