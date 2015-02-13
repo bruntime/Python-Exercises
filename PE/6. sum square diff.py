9f5d964ccd5f15cbf50af9765d0cd42a814ac76b
A Valembrun <bruntime@gmail.com>
2015-02-12 21:39:38 -0500

    PE Problem 6 (Python)
    - solved

--- PE/6. sum square diff.py
+++ PE/6. sum square diff.py
@@ -17,7 +17,7 @@ import math
 i = 0
 squares = []
 
-while i < 10:
+while i < 100:
 	i += 1
 	square_num = math.pow (i, 2)
 	squares.append(square_num)
@@ -26,5 +26,12 @@ sum_square_num = sum(squares)
 	
 print "First 10 numbers squared", squares
 
-# Next step: add first 10 natural numbers then square sum
-# Final step: Then subtract total sum of natural numbers by the squares of the natural numbers
+sum_nums = sum(range(1, 101))
+
+sum_nums_squared = math.pow(sum_nums, 2)
+
+print sum_nums_squared
+
+diff = sum_nums_squared - sum_square_num
+
+print "Difference of first one hundred natural numbers and the square of the sum is", diff
