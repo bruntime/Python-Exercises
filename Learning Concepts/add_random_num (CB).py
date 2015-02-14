f6d8b8f09610216f4ed825724a34e2a05448396d
A Valembrun <bruntime@gmail.com>
2015-02-13 21:13:43 -0500

    Folder Change
    - moved items
    - renamed items

--- Learning Concepts/add_random_num (CB).py
+++ Learning Concepts/add_random_num (CB).py
@@ -1,4 +1,4 @@
-# have the function SimpleAdding(num) add up all the numbers from 1 to num. For the test cases, the parameter num will be any number from 1 to 1000. 
+# have the function SimpleAdding(num) add up all the numbers from 1 to num.
 
 def SimpleAdding(num):
 
