47a824e76e1c1ef755c709a5d1ae9e218ccd6672
A Valembrun <bruntime@gmail.com>
2015-02-13 21:10:48 -0500

    Exercise - Additive Persistence (Python)
    - Coder Byte exercise
    - in progress

---
+++ Learning Concepts/additive_persistence (CB).py
@@ -0,0 +1,16 @@
+# Return a number's additive persistence which is the number of times you must add the digits until you reach a single digit. 
+# For example: if num is 2718 then your program should return 2 because 2 + 7 + 1 + 8 = 18 and 1 + 8 = 9 and you stop at 9
+
+def Add_Persist(num):
+
+	num_list = [int(i) for i in str(num)]
+
+	for i in num_list:
+		while i in num_list > 1:
+			sum_digits = sum(num_list)
+			num_list.append(sum_digits)
+			print sum(num_list)
+	
+number = int(raw_input("Give me a number: "))
+
+Add_Persist(number)
