6132cf3c93b86e38ce2cc6c0be2883ddb2268809
A Valembrun <bruntime@gmail.com>
2015-02-23 23:07:25 -0500

    Exercise - Counting Minutes (Python)
    - complete

--- Learning Concepts/counting_minutes.py
+++ Learning Concepts/counting_minutes.py
@@ -6,11 +6,12 @@ first_minute = int(raw_input("First Minutes: "))
 second_hour = int(raw_input("Second Hour: "))
 second_minute = int(raw_input("Second Minutes: "))
 total_hour = second_hour - first_hour
-total_minute = second_minute - first_minute
+hour_to_minutes = total_hour * 60
+total_minute = (second_minute - first_minute) + hour_to_minutes
 		
 if first_hour < 25 and first_minute < 60:
 	if second_hour < 25 and second_minute < 60:		
-		print "Time between two times: %d:%d" %(total_hour, total_minute)
+		print "Total minutes:%d minutes" %(total_minute)
 		#return total number of minutes between the times
 else:
 	print "invalid time"
