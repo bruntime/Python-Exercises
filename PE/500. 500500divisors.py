f628da992bdd70743761d188e15e5f13fbbddeb9
A Valembrun <bruntime@gmail.com>
2015-02-27 22:02:54 -0500

    PE Problem 500 (Python)
    - in progress

--- PE/500. 500500divisors.py
+++ PE/500. 500500divisors.py
@@ -2,23 +2,25 @@
 
 #Problem #500
 
-#The number of divisors of 120 is 16.
-#In fact 120 is the smallest number having 16 divisors.
+#The number of divisors of 120 is 16. In fact 120 is the smallest number having 16 divisors.
 
-#Find the smallest number with 2^500500 divisors.
-#Give your answer modulo 500500507.
+#Find the smallest number with 2^500500 divisors. Give your answer modulo 500500507.
 
-divisor = 0
-factors = []
-num = (len(factors)== 500)
+divisors = []
 
-for x in range(1, num):
-	while x < num:
-		divisor += 1
-		while divisor <= num:
-			if num % divisor == 0:
-				factors.append(divisor)
-			elif num % divisor != 0:
+a = -1
+
+while len(divisors) < 20:
+	a += 1
+	for x in range(1+a, 100):  
+		for y in range(1,(x/2)): #define range of numbers to divide x by
+			if x % y == 0:
+				divisors = divisors + [x] #append the new numbers to the list
+				if len(divisors) != 19:
+					divisors[:] #erase contents of list
+					continue
+			else:
+				print divisors
+				print len(divisors)
 				break
-			
-print (factors)
+					
