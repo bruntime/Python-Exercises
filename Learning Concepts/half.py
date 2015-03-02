64dad0d0903becb5ae1bd9585079816fcf4ba09a
A Valembrun <bruntime@gmail.com>
2015-03-01 21:13:14 -0500

    Small Exercise (Python)
    - updated exercise, complete (working)

--- Learning Concepts/half.py
+++ Learning Concepts/half.py
@@ -1,13 +1,14 @@
 #Divide number by two to make a new number.
-
 #example: 100, the screen output should be - 100, 50, 25, 12.5 ....
 
 def half(num):
 
-    while num > 0:
-      divide_two = num / 2
-      print divide_two
+	nums = []
+	
+	while num > 0.02:
+		nums.append(num)
+		num = num/2
+	print nums
 
-
-user_num = raw_input("number: ")
+user_num = float(raw_input("number: "))
 half(user_num)
