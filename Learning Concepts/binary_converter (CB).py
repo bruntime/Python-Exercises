8959200fbe6427ba01e375edf3144c12d2cf3a07
A Valembrun <bruntime@gmail.com>
2015-03-06 22:55:23 -0500

    Binary/Decimal Converter (Python)
    - in progress: binary conversion needed
    - decimal conversion complete (need to shorten code)

--- Learning Concepts/binary_converter (CB).py
+++ Learning Concepts/binary_converter (CB).py
@@ -10,13 +10,9 @@ def decimal_converter(num):
 	binary_nums = list(num)
 	binary_convert = []
 	
-	for x in binary_nums:
-			print x
-	print binary_nums
-	
 #index 0
 	if binary_nums[0] == "1":
-		place_0 = pow(2, 8)
+		place_0 = pow(2, 7)
 		binary_convert = binary_convert + [place_0]
 	elif binary_nums[0] == "0":
 		zero = 0
@@ -24,7 +20,7 @@ def decimal_converter(num):
 
 #index 1
 	if binary_nums[1] == "1":
-		place_1 = pow(2, 7)
+		place_1 = pow(2, 6)
 		binary_convert = binary_convert + [place_1]
 	elif binary_nums[1] == "0":
 		zero = 0
@@ -32,7 +28,7 @@ def decimal_converter(num):
 
 #index 2
 	if binary_nums[2] == "1":
-		place_2 = pow(2, 6)
+		place_2 = pow(2, 5)
 		binary_convert = binary_convert + [place_2]
 	elif binary_nums[2] == "0":
 		zero = 0
@@ -40,7 +36,7 @@ def decimal_converter(num):
 
 #index 3
 	if binary_nums[3] == "1":
-		place_3 = pow(2, 5)
+		place_3 = pow(2, 4)
 		binary_convert = binary_convert + [place_3]
 	elif binary_nums[3] == "0":
 		zero = 0
@@ -48,7 +44,7 @@ def decimal_converter(num):
 
 #index 4
 	if binary_nums[4] == "1":
-		place_4 = pow(2, 4)
+		place_4 = pow(2, 3)
 		binary_convert = binary_convert + [place_4]
 	elif binary_nums[4] == "0":
 		zero = 0
@@ -56,7 +52,7 @@ def decimal_converter(num):
 
 #index 5
 	if binary_nums[5] == "1":
-		place_5 = pow(2, 3)
+		place_5 = pow(2, 2)
 		binary_convert = binary_convert + [place_5]
 	elif binary_nums[1] == "0":
 		zero = 0
@@ -64,7 +60,7 @@ def decimal_converter(num):
 		
 #index 6
 	if binary_nums[6] == "1":
-		place_6 = pow(2, 2)
+		place_6 = pow(2, 1)
 		binary_convert = binary_convert + [place_6]
 	elif binary_nums[6] == "0":
 		zero = 0
@@ -72,13 +68,14 @@ def decimal_converter(num):
 		
 #index 7
 	if binary_nums[7] == "1":
-		place_7 = pow(2, 1)
+		place_7 = pow(2, 0)
 		binary_convert = binary_convert + [place_7]
 	elif binary_nums[7] == "0":
 		zero = 0
 		binary_convert = binary_convert + [zero]
 		
-	print binary_convert		
+	print binary_convert
+	print sum(binary_convert)
 		
 binary_num = raw_input("Number (no more than 8 characters): ") [:8]#maximum user input (8 characters)
 decimal_converter(binary_num)
