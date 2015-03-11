3542e75d58a6a02c402857476fe1dbdf10c2a8ca
A Valembrun <bruntime@gmail.com>
2015-03-10 21:58:08 -0400

    Exercise - Dictionary (Python)
    - complete

--- Learning Concepts/switch_txt_num.py
+++ Learning Concepts/switch_txt_num.py
@@ -3,18 +3,47 @@
 
 def txt_num(char):
 
-	d = dict()
+	letters = {
+	'a': 1, 
+	'b': 2, 
+	'c': 3, 
+	'd': 4, 
+	'e': 5,
+	'f': 6,
+	'g': 7,
+	'h': 8,
+	'i': 9,
+	'j': 10,
+	'k': 11,
+	'l': 12,
+	'm': 13,
+	'n': 14,
+	'o': 15,
+	'p':16,
+	'q':17,
+	'r':18,
+	's':19,
+	't':20,
+	'u':21,
+	'v':22,
+	'w':23,
+	'x':24,
+	'y':25,
+	'z':26}
 	
-	for c in char:
-		if c not in d:
-			d[c] = 1
-		else:
-			d[c] += 1
-	print d
+	user_txt = list(char)
 	
-txt = raw_input("Characters: ")
-chars = list(txt)
+	print user_txt
+	
+	# letter_switch = []
+	
+	for x in user_txt:
+		print letters[x]
+		
+		#add x to letter_switch list
 
-txt_num(chars)
+txt = raw_input("Characters: ")
+
+txt_num(txt)
 
 #http://www.greenteapress.com/thinkpython/html/thinkpython012.html
