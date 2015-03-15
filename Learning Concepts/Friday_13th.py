74f3ef5eecbf04a4a28d371254d517061edfca9a
A Valembrun <bruntime@gmail.com>
2015-03-14 23:53:25 -0400

    Exercise (Challenge) - Friday the 13th (Python)
    - in progress
    - latest version

--- Learning Concepts/Friday_13th.py
+++ Learning Concepts/Friday_13th.py
@@ -3,19 +3,20 @@
 import datetime
 
 # month = int(raw_input("Give a month: "))
-year = int(raw_input("Give a year: "))
 
 days_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 month = 0
 
-while month <= 12:
-	month += 1
-	date = days_of_week[datetime.date(year, (month+1), 13).weekday()]
-	print date
-
-if date == "Fri":
-	print "Friday the 13th"
-else:
-	print "Not Friday the 13th"
-
+attempt = raw_input("Would you like to give a year?: ")
+	while attempt == 'Y' or attempt == 'y':
+		year = int(raw_input("Give a year: "))
+		while year < 5000:
+			while month <= 11:
+				month += 1
+				date = days_of_week[datetime.date(year, (month), 13).weekday()]
+			if date == "Fri":
+				print datetime.date(year, (month), 13), "FRIDAY THE 13TH"
+			else:
+				print datetime.date(year, (month), 13)
+		break
 #next challenge: print all Friday the 13ths in a given year/time period
