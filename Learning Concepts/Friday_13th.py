25eca95ba298539b3a508c7c334f85d7eacbdf06
A Valembrun <bruntime@gmail.com>
2015-03-15 00:09:05 -0400

    Exercise (Challenge) - Friday the 13th (Python)
    - almost complete
    - issue with while loop (attempt)

--- Learning Concepts/Friday_13th.py
+++ Learning Concepts/Friday_13th.py
@@ -2,21 +2,20 @@
 
 import datetime
 
-# month = int(raw_input("Give a month: "))
-
 days_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+attempt = raw_input("Would you like to give a year? (Y or N): ")
+
 month = 0
 
-attempt = raw_input("Would you like to give a year?: ")
-	while attempt == 'Y' or attempt == 'y':
-		year = int(raw_input("Give a year: "))
-		while year < 5000:
-			while month <= 11:
-				month += 1
-				date = days_of_week[datetime.date(year, (month), 13).weekday()]
-			if date == "Fri":
-				print datetime.date(year, (month), 13), "FRIDAY THE 13TH"
-			else:
-				print datetime.date(year, (month), 13)
-		break
+while attempt == 'Y' or attempt == 'y':
+	year = int(raw_input("Give a year: "))
+	while month <= 11:
+		month += 1
+		date = days_of_week[datetime.date(year, (month), 13).weekday()]
+		if date == "Fri":
+			print datetime.date(year, (month), 13), "FRIDAY THE 13TH"
+		else:
+			print datetime.date(year, (month), 13)
+			
 #next challenge: print all Friday the 13ths in a given year/time period
