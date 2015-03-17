c9c050376f89cda642bdb0928f9d0f70dee939cc
A Valembrun <bruntime@gmail.com>
2015-03-16 22:02:14 -0400

    Exercise (Challenge) - Friday the 13th (Python)
    - updated to use a function
    - still having issues with while loop

--- Learning Concepts/Friday_13th.py
+++ Learning Concepts/Friday_13th.py
@@ -2,20 +2,23 @@
 
 import datetime
 
-days_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+def check_Fri_13th(user_year):
+
+	days_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+	month = 0
+
+	while month <= 11:
+		month += 1
+		date = days_of_week[datetime.date(user_year, (month), 13).weekday()]
+		if date == "Fri":
+			print datetime.date(user_year, (month), 13), "FRIDAY THE 13TH"
+		else:
+			print datetime.date(user_year, (month), 13)
 
 attempt = raw_input("Would you like to give a year? (Y or N): ")
 
-month = 0
+year = int(raw_input("Give a year: "))
 
-while attempt == 'Y' or attempt == 'y':
-	year = int(raw_input("Give a year: "))
-	while month <= 11:
-		month += 1
-		date = days_of_week[datetime.date(year, (month), 13).weekday()]
-		if date == "Fri":
-			print datetime.date(year, (month), 13), "FRIDAY THE 13TH"
-		else:
-			print datetime.date(year, (month), 13)
-			
-#next challenge: print all Friday the 13ths in a given year/time period
+while attempt == 'Y' or attempt == 'y': #causes infinite loop, need to figure out how to ask question until a no is received
+	check_Fri_13th(year)
