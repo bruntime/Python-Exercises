bb8b5aa27abece987b2438918fea0fe200a92e23
A Valembrun <bruntime@gmail.com>
2015-03-18 22:58:22 -0400

    Exercise - Reverse Print (Python)
    - in progress
    - tested, not working

--- Learning Concepts/reverse_strings.py
+++ Learning Concepts/reverse_strings.py
@@ -14,10 +14,14 @@ def reversing_txt(user_input):
 	
 	all_txt = all_txt + [user_input]
 
+	for x in reversed(all_txt):
+		print x
+	
 print "Type done when complete"
 
-txt = raw_input("What would you like to say? ")
+count = 0
 
-while txt != 'done':
-	print reversing_txt(txt)
-	
+while count < 10:
+	count += 1
+	txt = raw_input("What would you like to say? ")
+	reversing_txt(txt)
