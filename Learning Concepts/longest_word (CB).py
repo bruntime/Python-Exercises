c9aae02a420e9d1c16f4cb03af2d27c45712ed38
A Valembrun <bruntime@gmail.com>
2015-03-23 22:55:40 -0400

    Exercise - Longest Word (Python)
    - improvements

--- Learning Concepts/longest_word (CB).py
+++ Learning Concepts/longest_word (CB).py
@@ -3,28 +3,32 @@
  #Ignore punctuation and assume sen will not be empty.
  
 import string
- 
+
+def LongestWord(sen):
+
+#1. remove punctuation marks
+	for x in string.punctuation:
+		sen = sen.replace(x, "")
+
+#2. place user text in list and separate text into words	
+	sentence = sen.split(" ")
+
+#3. keep version of list
+	oldsentence = sentence
+
+#4. compare length of each word
+
+	while len(sentence) != 1: #loops until only 1 word left
+		if len(sentence[0]) >= len(sentence[1]):
+			sentence.remove(sentence[1])
+		elif len(sentence[0]) < len(sentence[1]):
+			sentence.remove(sentence[0])
+
+	for x in sentence:		
+		print "Longest word in sentence:", x
+
 print "\nProgram to Determine Longest Word in Sentence\n"
  
 user_text = raw_input("Write sentence here: ")
 
-#1. remove punctuation marks
-for x in string.punctuation:
-	user_text = user_text.replace(x, "")
-
-#2. place user text in list and separate text into words	
-sentence = user_text.split(" ")
-
-#3. keep version of list
-oldsentence = sentence
-
-#4. compare length of each word
-
-while len(sentence) != 1: #loops until only 1 word left
-	if len(sentence[0]) >= len(sentence[1]):
-		sentence.remove(sentence[1])
-	elif len(sentence[0]) < len(sentence[1]):
-		sentence.remove(sentence[0])
-
-for x in sentence:		
-	print "Longest word in sentence:", x
+LongestWord(user_text)
