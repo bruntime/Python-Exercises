73cc09fb886973e62a91ef9ebf906ee962aaeac2
A Valembrun <bruntime@gmail.com>
2015-03-26 22:59:19 -0400

    Exercise - Greatest Letter Count (Python)
    - in progress
    - Coder Byte exercise

---
+++ Learning Concepts/letter_count (CB).py
@@ -0,0 +1,17 @@
+def LetterCount(txt):
+
+	characters = list(txt)
+	characters.sort()
+	print characters
+
+	for x in characters:
+		if characters[a] == characters[b]:
+			
+user_txt = raw_input("word: ")
+
+LetterCount(user_txt)
+
+
+
+
+
