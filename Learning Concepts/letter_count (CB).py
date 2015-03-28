b2a3b2bb01708ca48a2c08c062146b7c45156ef3
A Valembrun <bruntime@gmail.com>
2015-03-27 23:12:38 -0400

    Exercise - Greatest Letter Count (Python)
    -in progress

--- Learning Concepts/letter_count (CB).py
+++ Learning Concepts/letter_count (CB).py
@@ -1,17 +1,32 @@
 def LetterCount(txt):
 
-	characters = list(txt)
-	characters.sort()
-	print characters
-
-	for x in characters:
-		if characters[a] == characters[b]:
-			
-user_txt = raw_input("word: ")
+	word =[]
+	word = txt.split(" ")
+	
+	#sort characters in word
+	
+	char_list = list(word[0])
+	print char_list
+	char_list.sort()
+	print char_list
+	
+	# a = -1
+	# b = 0
+	# count = 0
+	
+	#compare each character in a word
+	#count how many times a specific character shows up in the word
+	#if one character shows up more times than another, print count and character
+	
+	# while count < char_list[-1]:
+		# if char_list[a] == char_list[b]:
+			# a += 1
+			# b += 1
+			# count += 1
+			# print count
+		# else:
+			# return
+	
+user_txt = raw_input("sentence: ")
 
 LetterCount(user_txt)
-
-
-
-
-
