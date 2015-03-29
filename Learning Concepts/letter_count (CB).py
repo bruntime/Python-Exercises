fa784b4657fd3e96b4b9f8d2b4102d04c4bf7746
A Valembrun <bruntime@gmail.com>
2015-03-28 22:40:03 -0400

    Exercise - Greatest Letter Count (Python)
    - in progress

--- Learning Concepts/letter_count (CB).py
+++ Learning Concepts/letter_count (CB).py
@@ -10,22 +10,16 @@ def LetterCount(txt):
 	char_list.sort()
 	print char_list
 	
-	# a = -1
-	# b = 0
-	# count = 0
+	letters = {'a': 0,  'b': 0,  'c': 0,  'd': 0,  'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
 	
-	#compare each character in a word
-	#count how many times a specific character shows up in the word
-	#if one character shows up more times than another, print count and character
+	count = 0
 	
-	# while count < char_list[-1]:
-		# if char_list[a] == char_list[b]:
-			# a += 1
-			# b += 1
-			# count += 1
-			# print count
-		# else:
-			# return
+	#update dictionary with letter count
+	for x in char_list:
+		if x == letters['a']:
+			count += 1
+			letters['a'] = count
+			print count
 	
 user_txt = raw_input("sentence: ")
 
