651846eb109c876c29eefd62d3ad98b02d28cd2a
A Valembrun <bruntime@gmail.com>
2015-03-30 22:45:54 -0400

    Exercise - Fibonacci Checker (Python)
    - complete
    - Coder Byte exercise

---
+++ CB/fibonacci_check (CB).py
@@ -0,0 +1,30 @@
+# FibonacciChecker(num) return the string yes if the number given is part of the Fibonacci sequence. 
+#This sequence is defined by: Fn = Fn-1 + Fn-2, which means to find Fn you add the previous two numbers up. 
+#The first two numbers are 0 and 1, then comes 1, 2, 3, 5 etc. 
+#If num is not in the Fibonacci sequence, return the string no.
+
+def Fibonacci_Check(num):
+
+	list_nums = [0, 1]
+
+	a = -1
+	b = 0
+
+	while list_nums[-1] <= num:
+
+		a += 1
+		b += 1
+	
+		sum = list_nums[a] + list_nums[b]
+		list_nums.append(sum)
+	
+	print list_nums
+	
+	if num not in list_nums:
+		print 'no'
+	else:
+		print 'yes'
+
+user_num = int(raw_input("Check if what number is in Fibonacci series: "))
+
+Fibonacci_Check(user_num)
