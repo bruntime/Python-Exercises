8d5e414f4aa30c3c209665aecdf4ee41f9b9720f
A Valembrun <bruntime@gmail.com>
2015-04-01 23:16:10 -0400

    Exercise - Number Search (Python)
    - (edited) program instructions
    - bonus: complete as required according to Coder Byte exercise

--- CB/number_search (CB).py
+++ CB/number_search (CB).py
@@ -1,8 +1,4 @@
-# Search for all the numbers in the string, add them together, then return that final number. 
-# For example: if str is "88Hello 3World!" the output should be 91. 
-# You will have to differentiate between single digit numbers and multiple digit numbers like in the example above. 
-# So "55Hello" and "5Hello 5" should return two different answers. 
-# Each string will contain at least one letter or symbol. 
+# Search for all the numbers in the string, add them together, then return that final number
 
 def number_search(txt):
 
