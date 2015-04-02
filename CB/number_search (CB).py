129f8fc0aebacca0c40ddcd56641e53e96154708
A Valembrun <bruntime@gmail.com>
2015-04-01 23:11:24 -0400

    Exercise - Number Search (Python)
    - complete

--- CB/number_search (CB).py
+++ CB/number_search (CB).py
@@ -6,18 +6,19 @@
 
 def number_search(txt):
 
-	numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+	# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 	
-	characters = []
-	characters = txt.split(" ")
+	characters = list(txt)
+	
+	print characters
 	
 	numbers_from_txt = []
 	
-	a = 0
-	
 	for x in characters:
-		if x == a in numbers:
-			print x
+		if x == '1' or x == '2' or x == '3'or x == '4' or x == '5' or x == '6' or x == '7' or x == '8' or x == '9' or x == '0':
+			numbers_from_txt.append(int(x))
+	print numbers_from_txt
+	print sum(numbers_from_txt)
 
 sentence = raw_input("Sentence here (with numbers): ")
 
