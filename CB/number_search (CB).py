dd8b4752e7206f80e23370a730d8a7122701448d
A Valembrun <bruntime@gmail.com>
2015-04-02 01:03:16 -0400

    Exercise - Number Search (Python)
    - in progress
    - rounding issues
    - Coder Byte exercise

--- CB/number_search (CB).py
+++ CB/number_search (CB).py
@@ -1,19 +1,24 @@
-# Search for all the numbers in the string, add them together, then return that final number
+from decimal import decimal
 
-def number_search(txt):
+def NumberSearch(str): 
 
-	characters = list(txt)
+  	str = "".join(c for c in str if c not in ('!','.',':', '?', '"', '(', ')', '^', '&', '?', '`', '~', ' ', ','))
+	
+	characters = list(str)
+	letters = list(c for c in str if c not in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'))
 	
 	print characters
+	print letters
 	
 	numbers_from_txt = []
 	
 	for x in characters:
 		if x == '1' or x == '2' or x == '3'or x == '4' or x == '5' or x == '6' or x == '7' or x == '8' or x == '9' or x == '0':
 			numbers_from_txt.append(int(x))
-	print numbers_from_txt
 	print sum(numbers_from_txt)
+	print len(characters)
+	print decimal(sum(numbers_from_txt)/len(letters))
+    
+user_txt = raw_input('sentence: ')
 
-sentence = raw_input("Sentence here (with numbers): ")
-
-number_search(sentence)
+NumberSearch(user_txt)
