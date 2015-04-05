12c379bc918eba30814a2209263f2a0b2d602c26
A Valembrun <bruntime@gmail.com>
2015-04-04 21:00:54 -0400

    Exercise - Formatted Division (Python)
    - in progress
    - Coder Byte exercise

---
+++ CB/formatted_division (CB).py
@@ -0,0 +1,17 @@
+# FormattedDivision(num1,num2) take both parameters being passed, divide num1 by num2.
+# Return the result as a string with properly formatted commas and 4 significant digits after the decimal place. 
+# For example: if num1 is 123456789 and num2 is 10000 the output should be "12,345.6789". 
+# The output must contain a number in the one's place even if it is a zero.
+
+def format_division(num1, num2):
+
+	total = float(num1/num2)
+	
+	print total
+	
+	print (":.4f ".format(total))
+
+user_num1 = int(raw_input("Number 1: ")) 
+user_num2 = int(raw_input("Number 2: "))
+
+format_division(user_num1, user_num2)
