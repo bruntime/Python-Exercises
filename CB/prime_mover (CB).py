06b006c443555af1ab3618ef7e34eb71d2e271b3
A Valembrun <bruntime@gmail.com>
2015-04-05 22:28:30 -0400

    Revert "Exercise - Nth Prime (Python)"
    This reverts commit e0a53d14aafd4f9f78b76a4e29631aa8d5c4095a.
    
    Conflicts:
    	CB/prime_mover (CB).py

--- CB/prime_mover (CB).py
+++ CB/prime_mover (CB).py
@@ -3,20 +3,40 @@
 
 def prime_pick(num):
 
+<<<<<<< HEAD
 	prime = []
 	prime_total = len(prime)
+=======
+	nums = []
+	
+>>>>>>> parent of e0a53d1... Exercise - Nth Prime (Python)
 	a = 0
 	while len(prime) != 10:
 		a += 1
 		if num % a == 0:
-		 prime = prime + [a]
-	if len(prime) > 2:
+		 nums.append(a)
+	if len(nums) > 2:
 		return False
 	else:
 		return True
+<<<<<<< HEAD
 	
 	print prime
 	
 user_num = int(raw_input("nth prime pick: "))	
 
 prime_pick(user_num)
+=======
+
+prime_nums = []
+	
+user_num = int(raw_input("Your number: "))
+prime_num_select = int(raw_input("Prime Number Selection: "))
+
+for x in xrange(2, user_num+1):
+	if prime_pick(x):
+		prime_nums = prime_nums + [x]
+
+print prime_nums
+print prime_nums[prime_num_select-1]
+>>>>>>> parent of e0a53d1... Exercise - Nth Prime (Python)
