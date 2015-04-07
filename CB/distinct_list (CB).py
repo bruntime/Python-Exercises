4f15d139d006cec3c7af87bbfa0f73da12656170
A Valembrun <bruntime@gmail.com>
2015-04-06 22:55:54 -0400

    Exercise - Distinct List (Python)
    - in progress
    - Coder Byte exercise

---
+++ CB/distinct_list (CB).py
@@ -0,0 +1,18 @@
+# Take an array of numbers and determine the total number of duplicate entries. 
+# For example if the input is [1, 2, 2, 2, 3] then your program should output 2 because there are two duplicates of one of the elements. 
+
+# def dinstinct_list(arr):
+
+list_of_nums = [1, 2, 2, 2, 3]:
+
+count = 0
+
+a = -1
+b = 0
+		
+while list_of_nums[a] = list_of_nums[-1]:
+	a += 1
+	b += 1
+	if list_of_nums[a] == list_of_nums[b]:
+		count += 1
+		print count
