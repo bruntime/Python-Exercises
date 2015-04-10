fdc7834fd873176a8eee9bf607c38dbe6b60e696
A Valembrun <bruntime@gmail.com>
2015-04-09 22:30:21 -0400

    Exercise - Nth Prime (Python)
    - complete
    - alternative working version

--- CB/nth_prime (CB).py
+++ CB/nth_prime (CB).py
@@ -5,15 +5,16 @@ def prime_pick(n):
 
 	prime = []
 	
-	while len(prime) != n:
-		for num in range(1, 10):
-			if num > 1:
-				for i in range (2, num):
-					if (num % i) == 0:
-						break
-				else:
-					prime = prime + [num]
-					print prime
+	for num in range(1, 10000):
+		if num > 1:
+			for i in range (2, num):
+				if (num % i) == 0:
+					break
+			else:
+				prime = prime + [num]
+	
+	print prime
+	print prime[n-1] #outputs nth prime number
 		
 user_num = int(raw_input("nth number: "))
 
