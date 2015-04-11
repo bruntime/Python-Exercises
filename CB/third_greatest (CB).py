12850fd32e6875cc8c58a347527f393fef23f133
A Valembrun <bruntime@gmail.com>
2015-04-10 22:59:39 -0400

    Exercise - Third Greatest (Python)
    - in progress
    - Coder Byte exercise

---
+++ CB/third_greatest (CB).py
@@ -0,0 +1,16 @@
+# Take an array of strings and return the third largest word within in. 
+# The array will have at least three strings and each string will only contain letters. 
+
+def third_longest(list_words):
+
+	for x in list_words:
+	#determine which x is longest
+
+user_list_words = []
+user_txt = 0
+
+while user_txt != 'done':
+	user_txt = raw_input("List words: ")
+
+third_longest(user_list_words)
+
