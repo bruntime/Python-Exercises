1d428784dd3111d8f6908b7745f75608df5f19ac
A Valembrun <bruntime@gmail.com>
2015-04-11 22:58:11 -0400

    Exercise - Third Greatest (Python)
    - in progress

--- CB/third_greatest (CB).py
+++ CB/third_greatest (CB).py
@@ -4,13 +4,16 @@
 def third_longest(list_words):
 
 	for x in list_words:
-	#determine which x is longest
+	# determine which x is longest
 
 user_list_words = []
 user_txt = 0
 
+count = 0
 while user_txt != 'done':
-	user_txt = raw_input("List words: ")
+	count +=1
+	user_txt = raw_input("A word: ")
+	if count < 3:
+		print "You have to input  %d words" %(3 - count)
 
 third_longest(user_list_words)
-
