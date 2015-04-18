7998bda7ee94a715bbaed3b9093c22da85b3943e
A Valembrun <bruntime@gmail.com>
2015-04-18 15:03:52 -0400

    Exercise - Alphabetize (Python)
    - update (ignore numbers in string)
    - improving comments

--- CB/alphabetize (CB).py
+++ CB/alphabetize (CB).py
@@ -1,16 +1,19 @@
-#Return a string with the letters in alphabetical order (ie. hello becomes ehllo). 
-#Assume numbers and punctuation symbols will not be included in the string.
+# Program Name: Alphabetize Characters
+# Task Description: Return a string with the letters in alphabetical order.
+# Parameter: Numbers and punctuation symbols will not be included in the string.
+# Example: hello becomes ehllo
 
 def string_alphabetical_order(word):
 
-	word = "".join(c for c in word if c not in ('!','.',':', '?', '"', '(', ')', '^', '&', '?', '`', '~'))
-
+	#ignore punctuation symbols and numbers
+	word = "".join(c for c in word if c not in ('!','.',':', '?', '"', '(', ')', '^', '&', '?', '`', '~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'))
+	
 	chars = list(word)
-	print chars	
-	chars.sort()
+	chars.sort() #sorted characters in a list
 	
 	characters = []
 	
+	#rejoin characters as string
 	characters = "".join(x for x in chars)
 	print characters
 		
