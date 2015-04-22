f9f649f436704ba9ce8e11eeefbfcee735b5c774
A Valembrun <bruntime@gmail.com>
2015-04-21 23:20:46 -0400

    Exercise - Letter Count (Python)
    - in progress
    - retrying using new approach
    - improved commenting

--- CB/letter_count (CB).py
+++ CB/letter_count (CB).py
@@ -5,27 +5,19 @@
 
 def LetterCount(txt):
 
-	word =[]
-	word = txt.split(" ")
+	sentence = txt.split(" ")
+	sentence = [x.lower() for x in sentence]
+	print sentence
 	
-	#sort characters in word
-	
-	char_list = list(word[0])
-	print char_list
-	char_list.sort()
-	print char_list
-	
-	letters = {'a': 0,  'b': 0,  'c': 0,  'd': 0,  'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+	#separate characters in each word and place each set of characters in a list
+	characters = [list(x) for x in sentence]
+	print characters
 	
 	count = 0
 	
-	#update dictionary with letter count
-	for x in char_list:
-		if x == letters['a']:
-			count += 1
-			letters['a'] = count
-			print count
-	
+	#find repeating characters in each set
+	for y in characters:
+			
 user_txt = raw_input("sentence: ")
 
 LetterCount(user_txt)
