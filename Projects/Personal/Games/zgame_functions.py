1baf3b24975cf79a78b5341e2be925c21391bb97
A Valembrun <bruntime@gmail.com>
2015-04-23 15:28:49 -0400

    Folder Change
    - moved items
    - renamed items

--- Projects/Personal/Games/zgame_functions.py
+++
@@ -1,8 +0,0 @@
-def intro():
-	print '''
-		This is an extinction scale event.
-		Three years after the first infection, national governments have fallen. 
-		You find yourself running in the Portsmouth Naval Prison.
-		There are 20 zombies running after you and Valdez.
-		You manage to run inside and close the first gate but it won't hold them for long.
-	'''
