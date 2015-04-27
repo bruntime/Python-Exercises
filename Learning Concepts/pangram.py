928bbe7baf9a5cff0336a37129b9476029be426c
A Valembrun <bruntime@gmail.com>
2015-04-26 22:07:44 -0400

    Exercise - Pangram (Python)
    - in progress

---
+++ Learning Concepts/pangram.py
@@ -0,0 +1,23 @@
+# Program Name: Pangram or Not
+# Task Description: Check a sentence to see if it a pangram. (A pangram is a sentence that contains all the letters of the English alphabet at least once)
+# Parameter: N/A
+# Example: The quick brown fox jumps over the lazy dog.
+# Note: Problem 18
+
+def pangram(sentence):
+
+	letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+	characters = [x for x in sentence.split()]
+	
+	for n in characters:
+		for n in letters:
+			#compare characters and letters
+			#if all letters represented in characters
+				print "Is a pangram"
+			else:
+				print "Not a pangram"
+	
+user_txt = raw_input("Sentence: ")
+pangram(user_txt)
+
+#http://www.ling.gu.se/~lager/python_exercises.html
