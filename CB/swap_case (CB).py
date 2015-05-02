3b6238523ea15e1476c449dbe2c902300241a1a3
A Valembrun <bruntime@gmail.com>
2015-05-01 22:25:31 -0400

    Exercise - Swap Case (Python)
    - in progress
    - Coder Byte exercise

---
+++ CB/swap_case (CB).py
@@ -0,0 +1,21 @@
+# Program Name: Swap Letter Case
+# Task Description: Swap the case of each character
+# Parameter: Let numbers and symbols stay the way they are. 
+# Example: For "Hello World" the output should be hELLO wORLD.
+
+# def SwapCase(str):
+
+s = "One Two ThRee"
+
+words = s.split()
+
+a = 0
+
+while a < len(words):
+	a += 1
+	for x in words:
+		x.split()
+		if x.lower():
+			x.upper() #switch case
+		if x.upper:
+			x.lower() #switch case
