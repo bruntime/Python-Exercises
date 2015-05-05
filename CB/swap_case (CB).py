f27796fc5302436015640ae88b5a42c49a0fc568
A Valembrun <bruntime@gmail.com>
2015-05-04 23:12:48 -0400

    Exercise - Swap Case (Python)
    - working
    - issue: iterates through list with less 21 characters or more, need to
    fix in order to accept any length

--- CB/swap_case (CB).py
+++ CB/swap_case (CB).py
@@ -3,15 +3,26 @@
 # Parameter: Let numbers and symbols stay the way they are. 
 # Example: For "Hello World" the output should be hELLO wORLD.
 
-def SwapCase(sentence):
-   
-    uppercase = [x for x in sentence if x.isupper()]
-    lowercase = [x for x in sentence if x.islower()]
+def SwapCase(txt):
 
-    for x in uppercase:
-        print x.lower()
-    for x in lowercase:
-        print x.upper()
+	sentence = list(txt)
+
+	swapped_letters = []
+	
+	a = -1
+	
+	while a < 20:
+		a += 1
+		if sentence[a].isupper():
+			swapped_letters += [sentence[a].lower()]
+		else:
+			swapped_letters += [sentence[a].upper()]
+	
+	sentence = "".join(x for x in swapped_letters)
+	
+	print sentence
        
 user_txt = raw_input("Sentence: ")
 SwapCase(user_txt)
+
+#http://coderbyte.com/CodingArea/GuestEditor.php?ct=Swap%20Case&lan=Python
