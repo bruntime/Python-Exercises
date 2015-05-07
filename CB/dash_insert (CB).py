3110cebb9cad2aaa7aa20cc64cdf087a57c9e805
A Valembrun <bruntime@gmail.com>
2015-05-06 22:36:42 -0400

    Exercise - Dash Insert (Python)
    - in progress

--- CB/dash_insert (CB).py
+++ CB/dash_insert (CB).py
@@ -1,28 +1,34 @@
 # Program Name: Dash Between Two Odd Numbers
 # Task Description:  Insert dashes ('-') between two odd numbers in string.
 # Parameter: Don't count zero as an odd number.
-# Example: If string is 454793 the output should be 4547-9-3.
+# Example: If str is 454793 the output should be 4547-9-3.
 
 def dash_insert(txt):
 
 	numbers = list(txt)
-	odd_numbers = [1, 3, 5, 7, 9]
+	odd_numbers = ['1', '3', '5', '7', '9']
 	print numbers
 	
 	a = -1
 	b = -1
 	
 	new_numbers = []
-	
-	for x in numbers:
+
+	while numbers[b] != numbers[-1]:
 		a +=1
 		b +=1
+
+		for x in numbers:
 		
 		#check if first AND second numbers are odd numbers
-		if numbers[a] and numbers[b] in odd_numbers:
+			if numbers[a] and numbers[b] in odd_numbers:
 		#if odd numbers, place dash between and save to new list
-			new_numbers = new_numbers + numbers[a] + '-' + numbers[b]
-			print new_numbers
+				new_numbers = new_numbers + numbers[a] + '-' + numbers[b]
+			else:
+				new_numbers = new_numbers + numbers[a] + numbers[b]
+	print new_numbers
 	
 user_txt = raw_input("Numbers: ")
 dash_insert(user_txt)
+
+#http://coderbyte.com/CodingArea/Editor.php?ct=Dash%20Insert&lan=Python
