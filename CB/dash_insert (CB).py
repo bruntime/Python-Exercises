7e8085ed7ddbe58bed1db1e68da00f4b93d6dc07
A Valembrun <bruntime@gmail.com>
2015-05-07 22:50:06 -0400

    Exercise - Dash Insert (Python)
    - in progress
    - unsure of what the problem is, debugging in progress

--- CB/dash_insert (CB).py
+++ CB/dash_insert (CB).py
@@ -1,31 +1,31 @@
 # Program Name: Dash Between Two Odd Numbers
 # Task Description:  Insert dashes ('-') between two odd numbers in string.
 # Parameter: Don't count zero as an odd number.
-# Example: If str is 454793 the output should be 4547-9-3.
+# Example: If string is 454793 the output should be 4547-9-3.
 
 def dash_insert(txt):
 
 	numbers = list(txt)
+	numbers.append('end')
 	odd_numbers = ['1', '3', '5', '7', '9']
 	print numbers
 	
 	a = -1
-	b = -1
+	b = 0
 	
 	new_numbers = []
 
-	while numbers[b] != numbers[-1]:
+	while numbers[a] != 'end':
 		a +=1
 		b +=1
-
-		for x in numbers:
+		print numbers[a], 'and', numbers[b] #testing values of 'a' and 'b' from list
 		
-		#check if first AND second numbers are odd numbers
-			if numbers[a] and numbers[b] in odd_numbers:
-		#if odd numbers, place dash between and save to new list
-				new_numbers = new_numbers + numbers[a] + '-' + numbers[b]
-			else:
-				new_numbers = new_numbers + numbers[a] + numbers[b]
+		# check if first AND second numbers are odd numbers
+		if numbers[a] and numbers[b] in odd_numbers:
+		# if odd numbers, place dash between and save to new list
+			new_numbers = new_numbers + numbers[a] + '-' + numbers[b]
+		else:
+			new_numbers = new_numbers + numbers[a] + numbers[b]
 	print new_numbers
 	
 user_txt = raw_input("Numbers: ")
