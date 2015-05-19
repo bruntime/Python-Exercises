f8d300948f736e962dbeb8f02a7b2693423b7ac6
A Valembrun <bruntime@gmail.com>
2015-05-18 23:58:55 -0400

    Exercise - Letter Change (Python)
    - in progress (just started)
    - Coder Byte exercise

---
+++ CB/letter_change (CB).py
@@ -0,0 +1,11 @@
+# Program Name: Letter Change
+# Task Description: Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
+# Parameter: N/A
+# Example: "fun times" becomes "gvO Ujnft!"
+
+def letter_change(str)
+
+
+user_input = input(" ")
+
+#http://coderbyte.com/CodingArea/Editor.php?ct=Letter%20Changes&lan=Python
