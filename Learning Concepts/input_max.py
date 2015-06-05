d8b8c85ff475a2b524c44998ce824a6d9bd41076
A Valembrun <bruntime@gmail.com>
2015-06-05 00:09:08 -0400

    Exercise - Input Max Check (Python)
    - in progress

--- Learning Concepts/input_max.py
+++ Learning Concepts/input_max.py
@@ -4,12 +4,29 @@
 #If input is palindrome display the string "palindrome", otherwise print the input in reverse order (case sensitive)
 
 txt = []
-user_txt = raw_input("Text: ")
+numbers_in_txt = []
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
 
 while len(txt) != 15:
+	user_txt = raw_input("Test: ")
 	txt = txt + [user_txt]
 	if user_txt == "":
+		txt.pop()
 		break
+	
+	# numbers_in_txt = []
+
+	# for x in list(user_txt):
+		# if x in numbers:
+			# numbers_in_txt.append(int(x))
+	# print sum(numbers_in_txt)		
+		
+	# if user_txt == "":
+		# break
+	# else:
+		# txt = txt + [user_txt]	
 
 print txt
-print len(txt)
+print "There were",  len(txt), "input(s)"
