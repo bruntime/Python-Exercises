012f1a67f11abc70107ab145789ed629022fd4bc
A Valembrun <bruntime@gmail.com>
2015-06-12 20:55:45 -0400

    Exercise - Decode a Web Page (Python)
    - in progress

---
+++ Learning Concepts/decode_webpage.py
@@ -0,0 +1,12 @@
+# Program Name: Decode a Web Page
+# Task Description:  print out a list of all the article titles on the New York Times homepage
+# Parameter: N/A
+#Example: N/A
+
+import requests
+
+url = "http://www.nytimes.com/"
+r = requests.get(http://www.nytimes.com/)
+r_html = r.text
+
+http://www.practicepython.org/exercise/2014/06/06/17-decode-a-web-page.html
