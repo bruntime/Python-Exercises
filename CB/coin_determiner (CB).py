988320ef13794aab1f6743ee37848b0567835eeb
A Valembrun <bruntime@gmail.com>
2015-07-03 18:58:05 -0400

    Exercise: Coin Determiner (Python)
    - Coder Byte exercise
    - determine combination that uses least number of coins

---
+++ CB/coin_determiner (CB).py
@@ -0,0 +1,13 @@
+# Program Name: Coin Determiner
+# Task Description: Take integers and return an integer output that will specify the least number of coins, that when added, equal the input integer.
+# Parameter: Integers range from 1 to 250, coins representing the integers 1, 5, 7, 9, and 11.
+# Example: If num is 16, then the output should be 2 because you can achieve the number 16 with the coins 9 and 7. If num is 25, then the output should be 3 because you can achieve 25 with either 11, 9, and 5 coins or with 9, 9, and 7 coins. 
+
+from itertools import combinations
+
+coins = [1, 5, 7, 9, 11]
+
+output = sum([map(list, combinations(coins, i)) for i in range(len(coins) + 1)], [])
+
+print output
+
