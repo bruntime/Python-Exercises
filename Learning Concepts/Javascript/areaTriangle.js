443b8d992831365b86c3c114a689be6936a54a90
A Valembrun <bruntime@gmail.com>
2015-08-06 18:11:06 -0400

    Exercise: Triangle Area Calculator (JavaScript)
    - complete

--- Learning Concepts/Javascript/areaTriangle.js
+++ Learning Concepts/Javascript/areaTriangle.js
@@ -2,7 +2,6 @@ function calculate() {
 	var side1 = document.getElementById("side1").value;
 	var side2 = document.getElementById("side2").value;
 	var side3 = document.getElementById("side3").value;
-	var area = (side1 * side2 * side3);
-	document.getElementById("area").innerHTML = area
-	alert area
-}
+	var area = side1 * side2 * side3;
+	document.getElementById("area").value = area;
+	}
